d9395d1d23acd4479716db3591f3f80020695714
DanielleM <dlmedeiro@gmail.com>
2022-06-03 14:25:47 -0700

    Updates made to connect server

--- config.js
+++ config.js
@@ -7,14 +7,24 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+// Changed from 3001 to 5432 - const error = this._ending ? new Error('Connection terminated') : new Error('Connection terminated unexpectedly')
+const PORT = +process.env.PORT || 5432;
 
-// Use dev database, testing database, or via env var, production database
+// Use dev database, testing database, or via env var, production database'
+
+
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+      ? `postgresql://postgres:${process.env.PASSWORD}@localhost:${PORT}/jobly_test`
+      : `postgresql://postgres:${process.env.PASSWORD}@localhost:${PORT}/jobly`;
 }
+
+// Initial
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
@@ -32,5 +42,6 @@ module.exports = {
   SECRET_KEY,
   PORT,
   BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
+  getDatabaseUri
+  // DB_URI,
 };
