e72349b822649536ba268bed18f2cfb6c351ba74
DanielleM <dlmedeiro@gmail.com>
2022-08-26 08:44:16 -0700

    clean documentation on sqlForPartialUpdate

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const DATABASE_URL = process.env.DATABASE_URL;
-const DATABASE_URL_TEST = process.env.DATABASE_URL_TEST;
+const DATABASE_URL = process.env.DATABASE_URL || "jobly";
+const DATABASE_URL_TEST = process.env.DATABASE_URL_TEST || "jobly_test";
 
 // Changed from 3001 to 5432 - const error = this._ending ? new Error('Connection terminated') : new Error('Connection terminated unexpectedly')
 const PORT = +process.env.PORT || 3001;
--- helpers/sql.js
+++ helpers/sql.js
@@ -3,29 +3,31 @@ const { BadRequestError } = require("../expressError");
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
 // Function takes in req.body as dataToUpdate which is passed through the companies/:handle patch route, into the update function on the companies model.
-// jsToSQL and the cols variable translate the numEmployees and logoUrl aliases into their equivelant SQL column titles.
-// the cols variable also creates a new array mapping the existing keys to their equivilant varialbe number (used back in the update function)
+// jsToSQL and the cols variable translate the numEmployees and logoUrl aliases into their equivalent SQL column titles.
+// the cols variable also creates a new array mapping the existing keys to their equivalent variable number (used back in the update function)
 
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   // console.log(`dataToUpdate = ${dataToUpdate}, jsToSql = ${jsToSql.numEmployees}`)
-  // dataToUpdate = [object Object], jsToSql = num_employees
+    // dataToUpdate = [object Object], jsToSql = num_employees
   const keys = Object.keys(dataToUpdate);
   // console.log(`keys = ${keys}`)
-  // keys = name,numEmployees,logoUrl
+    // keys = name,numEmployees,logoUrl
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
-  // console.log(`cols = ${cols}`)
-  // cols = "name"=$1,"num_employees"=$2,"logo_url"=$3
+    // console.log(`cols = ${cols}`)
+      // cols = "name"=$1,"num_employees"=$2,"logo_url"=$3
 
-  // console .log(`setCols = ${cols.join(", ")}`)
-  // setCols = "name"=$1, "num_employees"=$2, "logo_url"=$3
-  // console .log(`values = ${Object.values(dataToUpdate)}`)
-  // values = Baker-Santos-Aranda,300,https://ww...
+    // console .log(`setCols = ${cols.join(", ")}`)
+      // setCols = "name"=$1, "num_employees"=$2, "logo_url"=$3
+
+    // console .log(`values = ${Object.values(dataToUpdate)}`)
+      // values = Baker-Santos-Aranda,300,https://ww...
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
